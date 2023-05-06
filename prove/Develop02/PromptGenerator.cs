1c343cf641bd36fbac1e70d9ba91e0df97b6d763
byui-programmer-96 <lynnwriter96@gmail.com>
2023-05-05 22:39:22 -0500

    developer 2

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,15 +1,28 @@
-class PromptGenerator
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
 {
-    List<string> prompts;
+    public List<string> prompts;
 
     public PromptGenerator()
     {
+        prompts = new List<string>();
+        prompts.Add("What was the best thing that happened today?");
+        prompts.Add("What was the worst thing that happened today?");
+        prompts.Add("What was the most challenging thing I faced today?");
+        prompts.Add(" What did I learn today?");
+        prompts.Add("What did I do today that I am proud of?");
 
     }
 
     public string GetRandomPRompt()
     {
-        return "";
+        Random random = new Random();
+        int numberPrompts = random.Next(prompts.Count);
+        string randomPrompt = prompts[numberPrompts];
+        Console.WriteLine(randomPrompt);
+        return randomPrompt;
     }
 
     internal string GetRandomPrompt()
