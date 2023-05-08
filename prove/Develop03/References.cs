1460132ddf154169c779fcd5f0c8f7635f5371a4
byui-programmer-96 <lynnwriter96@gmail.com>
2023-05-08 14:39:40 -0500

    developer 3

---
+++ prove/Develop03/References.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Reference
+{
+    private string book,chapter,verseStart,verseEnd;
+
+    public Reference (string _book,string _chapter,string _verse);
+    public Reference (string _book,string _chapter,string _verseStart,string _verseEnd);
+
+    public string GetReference();
+
+    
+
+
+
+}
