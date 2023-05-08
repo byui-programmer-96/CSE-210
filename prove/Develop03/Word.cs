0673815dd86787090b5363770e35d980cdcfc1c5
byui-programmer-96 <lynnwriter96@gmail.com>
2023-05-08 14:47:26 -0500

    developer 3

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Word
+{
+    private string word;
+    private bool isHidden;
+
+    public Word(string _word);
+
+    public bool GetIsHidden();
+
+    public void SetIsHidden(bool _isHidden);
+
+    public string GetWord();
+
+}
